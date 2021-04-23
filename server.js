4cf5a01447fd709dcaf0d2e6e348457b8120b435
obotha <khyper234@gmail.com>
2021-04-23 05:38:54 +0300

    Serve JSON data.

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const http = require('http');
 
+const toDos = [
+  { id: 1, text: 'To Do one' },
+  { id: 2, text: 'To Do Two' },
+  { id: 3, text: 'To Do Three' },
+];
+
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Type', 'application/json');
   /** Custom headers start with X- */
   res.setHeader('X-Powered-By', 'Node.js');
-  res.write('<h1>Hello</h1>');
-  res.write('<h2>Hello</h2>');
-  res.end();
+  res.end(
+    JSON.stringify({
+      success: true,
+      data: toDos,
+    })
+  );
 });
 
 const PORT = 5000;
