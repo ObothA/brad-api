f47a028a1e1e90eadba0634e0b9802a18c425549
obotha <khyper234@gmail.com>
2021-04-23 05:32:45 +0300

    Add custom powered by header.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
+  /** Custom headers start with X- */
+  res.setHeader('X-Powered-By', 'Node.js');
   res.write('<h1>Hello</h1>');
   res.write('<h2>Hello</h2>');
   res.end();
