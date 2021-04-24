8e139c2f9d1a1634467bcc6e37e6a7f1ab02a8a6
obotha <khyper234@gmail.com>
2021-04-24 16:49:22 +0300

    Add custom logger to middlweware and use it.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,7 +9,6 @@ exports.getBootcamps = (req, res, next) => {
   res.status(200).json({
     success: true,
     msg: 'Get all bootcamps',
-    hello: req.hello,
   });
 };
 
---
+++ middleware/logger.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ * @description logs request to console
+ */
+const logger = (req, res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
+  );
+  next();
+};
+
+module.exports = logger;
