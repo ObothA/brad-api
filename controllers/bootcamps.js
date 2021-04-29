4f220118f1048450eed6023e5ebc282a29906782
obotha <khyper234@gmail.com>
2021-04-29 11:50:31 +0300

    Add json body parser.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 /**
  * @description Get all bootcamps
  * @param {*} req
@@ -34,6 +36,7 @@ exports.getBootcamp = (req, res, next) => {
  * @access Private
  */
 exports.createBootcamp = (req, res, next) => {
+  console.log(req.body);
   res.status(200).json({
     success: true,
     msg: 'Create new bootcamp',
