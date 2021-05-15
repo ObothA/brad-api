7f6e1d543adc2ebea49134743ad82c324b332cc6
obotha <khyper234@gmail.com>
2021-05-15 13:29:58 +0300

    Delete course from db.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This is a Node js API in express that has bootcamps. Each bootcamp has courses.
 Courses populate their corresponding bootcamp while bootcamps reverse populate their courses via mongoose.
 
 Use a mongoose "pre" hooks to delete courses that belong to a bootcamp if the bootcamp is being deleted
+
+Full CRUD operations on bootcamps and courses.
--- controllers/courses.js
+++ controllers/courses.js
@@ -109,10 +109,35 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     runValidators: true
   });
 
-  
-
   res.status(200).json({
     success: true,
     data: course,
   });
+});
+
+/**
+ * @description Delete  course
+ * @param {*} req
+ * @param {*} res
+ * @route DELETE /api/v1/courses/:id
+ * @access Private
+ */
+ exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+if (!course) {
+  return next(
+    new ErrorResponse(
+      `No course with the id of ${req.params.id}`,
+      404
+    )
+  );
+}
+
+await course.remove();
+
+res.status(200).json({
+  success: true,
+  data: {},
+});
 });
