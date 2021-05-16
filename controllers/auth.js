a2599f9c480ef1c758a2ee68080939c757d47855
obotha <khyper234@gmail.com>
2021-05-16 17:04:59 +0300

    Get me route

--- controllers/auth.js
+++ controllers/auth.js
@@ -78,3 +78,21 @@ const sendTokenResponse = (user, statusCode, res) => {
       token
     })
 };
+
+
+/**
+ * @description Get current logged in user
+ * @param {*} req
+ * @param {*} res
+ * @route POST /api/v1/auth/me
+ * @access Private
+ */
+
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    data: user
+  })
+})
