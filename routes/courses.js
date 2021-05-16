84989d067c912076b1c0fa56ce4e0e898a40ee85
obotha <khyper234@gmail.com>
2021-05-16 15:59:43 +0300

    Add protector function.

--- routes/courses.js
+++ routes/courses.js
@@ -10,6 +10,7 @@ const {
 
  const Course = require('../models/Course');
  const advancedResults = require('../middleware/advancedResults');
+ const { protect } = require('../middleware/auth');
 
 const router = express.Router({ mergeParams: true });
 
@@ -20,11 +21,11 @@ router.route('/')
       select: 'name description',
     }
   ), getCourses)
-  .post(addCourse);
+  .post(protect, addCourse);
 
 router.route('/:id')
   .get(getCourse)
-  .put(updateCourse)
-  .delete(deleteCourse);
+  .put(protect, updateCourse)
+  .delete(protect, deleteCourse);
 
 module.exports = router;
