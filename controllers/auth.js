20652a9ee0bd82c79bec190075fdb74ae254eff7
obotha <khyper234@gmail.com>
2021-05-23 17:17:06 +0300

    Update password.

--- controllers/auth.js
+++ controllers/auth.js
@@ -102,6 +102,23 @@ exports.updateDetails = asyncHandler(async (req, res, next) => {
   });
 });
 
+// @desc      Update password
+// @route     PUT /api/v1/auth/updatepassword
+// @access    Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+
+  // Check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse('Password is incorrect', 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
+
 exports.forgotPassword = asyncHandler(async (req, res, next) => {
   const user = await User.findOne({ email: req.body.email });
 
