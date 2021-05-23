f6fb820506b1eb4bf1f217e89f9de4edefdbe4a5
obotha <khyper234@gmail.com>
2021-05-23 17:06:53 +0300

    Update user details.

--- controllers/auth.js
+++ controllers/auth.js
@@ -82,6 +82,26 @@ exports.getMe = asyncHandler(async (req, res, next) => {
  * @access Public
  */
 
+// @desc      Update user details
+// @route     PUT /api/v1/auth/updatedetails
+// @access    Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
 exports.forgotPassword = asyncHandler(async (req, res, next) => {
   const user = await User.findOne({ email: req.body.email });
 
