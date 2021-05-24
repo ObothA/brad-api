f0167262632a569e76c4e0e3451b7d593a598ad0
obotha <khyper234@gmail.com>
2021-05-24 15:56:08 +0300

    Clean data of xss attacks

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const cookieParser = require('cookie-parser');
 /** Security */
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const xss = require('xss-clean');
 
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
@@ -46,6 +47,9 @@ app.use(mongoSanitize());
 
 // Set security headers
 app.use(helmet());
+
+// PRevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
