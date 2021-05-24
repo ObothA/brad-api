0d65d3db56508bc295a03f97c6a26d5a0f08d17e
obotha <khyper234@gmail.com>
2021-05-24 16:07:25 +0300

    Add rate limiting.

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
@@ -50,6 +52,14 @@ app.use(helmet());
 
 // PRevent XSS attacks
 app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100,
+});
+
+app.use(limiter);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
