f61d1874f562b2d9261ca9f98c00980ba6d2714c
obotha <khyper234@gmail.com>
2021-05-24 15:08:50 +0300

    Uncomment cookie handler.

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,15 +7,14 @@ const User = require('../models/User');
 exports.protect = asyncHandler(async (req, res, next) => {
   let token;
 
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
     token = req.headers.authorization.split(' ')[1];
-  } 
-  
-  // to use cookies, uncomment this
-  
-  // else if (req.cookies.token) {
-  //   token = req.cookies.token;
-  // } 
+  } else if (req.cookies.token) {
+    token = req.cookies.token;
+  }
 
   // Make sure token exists
   if (!token) {
@@ -36,14 +35,18 @@ exports.protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-
 // Grant accees to specific roles
 exports.authorize = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-      return next(new ErrorResponse(`User role ${req.user.role} is not unauthorized to access this route`, 403));
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is not unauthorized to access this route`,
+          403
+        )
+      );
     }
 
     next();
-  }
+  };
 };
